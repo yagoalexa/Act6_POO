a350a77d1fc9c62355e015a0809de14e86229ad6
Yesica Gomez <yago_alexa@hotmail.com>
2021-11-30 22:25:39 -0500

    Programa  inicial de POO

---
+++ ElevarNumero.java
@@ -0,0 +1,21 @@
+
+
+/**
+ * ElevarNumero
+ */
+public class ElevarNumero {
+    public static void main(String[] args) {
+        ElevaNumero(3,4);
+        ElevaNumero(3,5);
+        ElevaNumero(3,6);
+        ElevaNumero(3,7);
+        ElevaNumero(3,8);
+        System.out.println("Hola Mundo");
+    }
+
+
+    public static void ElevaNumero(int base, int exponente){
+        
+        System.out.println(Math.pow(base, exponente));
+    }
+}
