042dacc32b212e96ba0a65a91a24729cde6d961c
Alexandra Gomez <yago_alexa@hotmail.com>
2021-12-08 22:18:03 -0500

    Update ElevarNumero.java
    Se ajusta el código para que la elevación sea por contador.

--- ElevarNumero.java
+++ ElevarNumero.java
@@ -1,21 +1,29 @@
 
-
 /**
  * ElevarNumero
  */
 public class ElevarNumero {
     public static void main(String[] args) {
         ElevaNumero(3,4);
-        ElevaNumero(3,5);
+        ElevaNumero(3,1);
         ElevaNumero(3,6);
         ElevaNumero(3,7);
         ElevaNumero(3,8);
-        System.out.println("Hola Mundo");
     }
-
-
+    
     public static void ElevaNumero(int base, int exponente){
+        try {
+            int contador = exponente;
+            int numero=1;
+            while (contador >= 1) {
+                numero *= base;
+                contador--;
+            }
+            System.out.println(numero);
+        } catch (Exception e) {
+            System.out.println("Error; "+e.getMessage());
+        }
+
         
-        System.out.println(Math.pow(base, exponente));
     }
 }
